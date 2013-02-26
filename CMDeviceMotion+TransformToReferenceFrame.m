1e78cda5378a2d902fed95aabbb5dbcc60df6c54
Simon Bogutzky <simon.bogutzky@t-online.de>
2013-02-26 13:59:25 +0100

    Add new method from paper

--- CMDeviceMotion+TransformToReferenceFrame.m
+++ CMDeviceMotion+TransformToReferenceFrame.m
@@ -7,20 +7,35 @@
 //
 
 #import "CMDeviceMotion+TransformToReferenceFrame.h"
+#import <GLKit/GLKit.h>
 
 @implementation CMDeviceMotion (TransformToReferenceFrame)
 
 -(CMAcceleration)userAccelerationInReferenceFrame
 {
-    CMAcceleration acc = [self userAcceleration];
-    CMRotationMatrix rot = [self attitude].rotationMatrix;
+    // Get the attitude rotation matrix
+    CMRotationMatrix rotationMatrix = self.attitude.rotationMatrix;
+    GLKMatrix3 matrix = GLKMatrix3Make(rotationMatrix.m11, rotationMatrix.m12, rotationMatrix.m13, rotationMatrix.m21, rotationMatrix.m22, rotationMatrix.m23, rotationMatrix.m31, rotationMatrix.m32, rotationMatrix.m33);
+    NSLog(@"%@", NSStringFromGLKMatrix3(matrix));
     
-    CMAcceleration accRef;
-    accRef.x = acc.x*rot.m11 + acc.y*rot.m12 + acc.z*rot.m13;
-    accRef.y = acc.x*rot.m21 + acc.y*rot.m22 + acc.z*rot.m23;
-    accRef.z = acc.x*rot.m31 + acc.y*rot.m32 + acc.z*rot.m33;
+    // Compute the inverse matrix
+    bool *isInvertible = NULL;
+    GLKMatrix3 iMatrix = GLKMatrix3Invert(matrix, isInvertible);
+    NSLog(@"%@", NSStringFromGLKMatrix3(iMatrix));
+    NSLog(@"%@", isInvertible ? @"NO" : @"YES");
     
-    return accRef;
+    // Multiplying the inverse matrix with user acceleration vector.
+    CMAcceleration userAcceleration = self.userAcceleration;
+    GLKVector3 vector = GLKVector3Make(userAcceleration.x, userAcceleration.y, userAcceleration.y);
+    NSLog(@"%@", NSStringFromGLKVector3(vector));
+    GLKVector3 rVector = GLKMatrix3MultiplyVector3(iMatrix, vector);
+    NSLog(@"%@", NSStringFromGLKVector3(rVector));
+    CMAcceleration relativeAcceleration;
+    relativeAcceleration.x = rVector.x;
+    relativeAcceleration.y = rVector.y;
+    relativeAcceleration.z = rVector.z;
+    
+    return relativeAcceleration;
 }
 
 @end
