dcc3e3718661489d083492e66ecf82fae6bfddf7
Simon Bogutzky <simon.bogutzky@t-online.de>
2013-03-22 14:29:32 +0100

    We need always sound

--- AppDelegate.m
+++ AppDelegate.m
@@ -91,7 +91,6 @@
 							
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    _audioController.active = NO;
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
@@ -109,8 +108,6 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    _audioController.active = YES;
-
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
