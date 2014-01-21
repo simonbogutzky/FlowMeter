75e8e0c7c0e0da79da1493b2a3b5d478ae906bd1
Simon Bogutzky <simon.bogutzky@t-online.de>
2014-01-21 16:42:59 +0100

    LikertScaleViewController hinzugefügt

---
+++ DataCollector/classes/ui/LikertScaleViewController.h
@@ -0,0 +1,13 @@
+//
+//  LikertScaleViewController.h
+//  DataCollector
+//
+//  Created by Simon Bogutzky on 21.01.14.
+//  Copyright (c) 2014 Simon Bogutzky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LikertScaleViewController : UIViewController
+
+@end
---
+++ DataCollector/classes/ui/LikertScaleViewController.m
@@ -0,0 +1,25 @@
+//
+//  LikertScaleViewController.m
+//  DataCollector
+//
+//  Created by Simon Bogutzky on 21.01.14.
+//  Copyright (c) 2014 Simon Bogutzky. All rights reserved.
+//
+
+#import "LikertScaleViewController.h"
+
+@interface LikertScaleViewController ()
+
+@end
+
+@implementation LikertScaleViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end
