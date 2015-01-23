619f3b3300e29a39cd3073b925853b29df675514
Simon Bogutzky <simon.bogutzky@t-online.de>
2015-01-23 10:33:19 +0100

    Swift code entnommen

---
+++ FlowMeter/classes/ui/LaunchViewController.h
@@ -0,0 +1,13 @@
+//
+//  LaunchViewController.h
+//  FlowMeter
+//
+//  Created by Simon Bogutzky on 23.01.15.
+//  Copyright (c) 2015 Simon Bogutzky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LaunchViewController : UIViewController
+
+@end
